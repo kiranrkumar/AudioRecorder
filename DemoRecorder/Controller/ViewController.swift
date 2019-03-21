923eaa2d1547a490c37ae112672763839287f985
Kiran Kumar <krkumar11@gmail.com>
2019-03-20 23:56:31 -0700

    Replace record text button with image button

--- DemoRecorder/Controller/ViewController.swift
+++ DemoRecorder/Controller/ViewController.swift
@@ -21,15 +21,20 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDe
     var recorderAndPlayer : VoiceRecorderAndPlayer = VoiceRecorderAndPlayer.sharedInstance
     let playImage = UIImage(contentsOfFile:Bundle.main.path(forResource: "play", ofType: "png")!)
     let pauseImage = UIImage(contentsOfFile:Bundle.main.path(forResource: "pause", ofType: "png")!)
+    let recordImage = UIImage(contentsOfFile:Bundle.main.path(forResource: "record", ofType: "png")!)
+    let stopImage = UIImage(contentsOfFile:Bundle.main.path(forResource: "stop", ofType: "png")!)
+    
     let playImageID = "ButtonPlay"
     let pauseImageID = "ButtonPause"
+    let recordImageID = "ButtonRecord"
+    let stopImageID = "ButtonStop"
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        recordButton.layer.cornerRadius = 10;
         playPauseImageButton.isEnabled = false;
         playPauseImageButton.accessibilityIdentifier = playImageID
+        recordButton.accessibilityIdentifier = recordImageID
         
         NotificationCenter.default.addObserver(self, selector: #selector(_recordingDidStart(_:)), name: RecordingDidStartNotification, object: recorderAndPlayer)
         NotificationCenter.default.addObserver(self, selector: #selector(_recordingDidFinish(_:)), name: RecordingDidFinishNotification, object: recorderAndPlayer)
@@ -39,23 +44,23 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDe
     
     //MARK: Notification Responders
     @objc func _recordingDidStart(_ notification:Notification) {
-        recordButton.setTitle("Stop", for: .normal)
         playPauseImageButton.isEnabled = false;
+        updateButton(button: recordButton, image: stopImage!, identifer: stopImageID)
     }
     
     @objc func _recordingDidFinish(_ notification:Notification) {
-        recordButton.setTitle("Record", for: .normal)
+        updateButton(button: recordButton, image: recordImage!, identifer: recordImageID)
         playPauseImageButton.isEnabled = true;
     }
     
     @objc func _playbackDidStart(_ notification:Notification) {
         recordButton.isEnabled = false;
-        updateButton(image: pauseImage!, identifer: pauseImageID)
+        updateButton(button : playPauseImageButton, image: pauseImage!, identifer: pauseImageID)
     }
     
     @objc func _playbackDidFinish(_ notification:Notification) {
+        updateButton(button : playPauseImageButton, image: playImage!, identifer: playImageID)
         recordButton.isEnabled = true;
-        updateButton(image: playImage!, identifer: playImageID)
     }
     
     func getDocumentDirectory() -> URL {
@@ -87,9 +92,21 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDe
         }
     }
     
-    func updateButton(image : UIImage, identifer : String) {
-        playPauseImageButton.setImage(image, for: .normal)
-        playPauseImageButton.accessibilityIdentifier = identifer
+    @IBAction func recordTouchUp(_ sender: Any) {
+        let identifier = recordButton.accessibilityIdentifier!
+        if (identifier.elementsEqual(recordImageID)) {
+            recorderAndPlayer.record()
+        } else if (identifier.elementsEqual(stopImageID)) {
+            recorderAndPlayer.stopRecording()
+        } else {
+            assert(false, "ViewController::recordTouchUp -> Unexpected button identifier")
+        }
+    }
+    
+    
+    func updateButton(button : UIButton, image : UIImage, identifer : String) {
+        button.setImage(image, for: .normal)
+        button.accessibilityIdentifier = identifer
     }
 }
 
