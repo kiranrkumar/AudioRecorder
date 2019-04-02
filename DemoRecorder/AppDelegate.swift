e9dcbbfa8782b3293db7295c8475a17262596acc
Kiran Kumar <krkumar11@gmail.com>
2019-04-01 19:44:15 -0700

    Pause activity on resign active and enter background, and stop activity on app exit

--- DemoRecorder/AppDelegate.swift
+++ DemoRecorder/AppDelegate.swift
@@ -14,6 +14,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let recorderAndPlayer : VoiceRecorderAndPlayer = VoiceRecorderAndPlayer.sharedInstance
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -24,11 +25,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        pauseRecorderAndPlayerActivity()
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        pauseRecorderAndPlayerActivity()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -41,8 +44,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        stopRecorderAndPlayerActivity()
     }
 
+    func pauseRecorderAndPlayerActivity() {
+        if recorderAndPlayer.isPlaying() {
+            recorderAndPlayer.pausePlayback()
+        }
+        else if recorderAndPlayer.isRecording() {
+            recorderAndPlayer.stopRecording()
+        }
+    }
+    
+    func stopRecorderAndPlayerActivity() {
+        if recorderAndPlayer.isPlaying() {
+            recorderAndPlayer.stopPlayback()
+        }
+        else if recorderAndPlayer.isRecording() {
+            recorderAndPlayer.stopRecording()
+        }
+    }
 
 }
 
